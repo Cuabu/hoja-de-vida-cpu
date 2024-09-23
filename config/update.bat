01b212c9dbf9431a138902432c69f4703e8c4c33
unknown <albertocuaburodriguez@gmail.com>
2024-09-23 14:40:28 -0500

    actualizando el update

--- config/update.bat
+++ config/update.bat
@@ -1,16 +1,15 @@
 @echo off
 REM Cambia a la carpeta del proyecto en XAMPP
-cd C:\xampp\htdocs
+cd C:\xampp\htdocs\
 
 REM Verifica si el repositorio ya está clonado
 IF EXIST "hoja-de-vida-cpu" (
-    echo El repositorio ya está clonado. Actualizando...
-    cd hoja-de-vida-cpu
-    git pull
-) ELSE (
-    echo Clonando el repositorio...
-    git clone https://github.com/Cuabu/hoja-de-vida-cpu.git
+    echo El repositorio ya está clonado. Eliminando archivos antiguos...
+    rmdir /s /q hoja-de-vida-cpu
 )
+
+echo Clonando el repositorio...
+git clone https://github.com/Cuabu/hoja-de-vida-cpu.git
 
 REM Mensaje de confirmación
 echo El repositorio ha sido actualizado con éxito.
